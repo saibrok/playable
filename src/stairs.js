283c639a6235c13bf166b0db92fb5408db89c210
Sergey Litvin <litvinsergey@list.ru>
2021-08-28 23:17:59 +0200

    - добавил первые анимации на requestAnimationFrame - установил animejs

--- src/stairs.js
+++ src/stairs.js
@@ -1,3 +1,5 @@
+import anime from "animejs/lib/anime.es.js";
+
 const app = window.app;
 
 console.log(app);
@@ -7,6 +9,14 @@ app.stage.on("loaded", () => {
 });
 
 function start() {
+  anime({
+    targets: app.objects.hammer,
+    translateX: 250,
+    rotate: "1turn",
+    backgroundColor: "#FFF",
+    duration: 800,
+  });
+
   app.objects.hammer.on("pointerdown", onClickHammer(app.objects.hammer));
   app.objects.btnOk01.on("pointerdown", onClickOk);
   app.objects.btnOk02.on("pointerdown", onClickOk);
@@ -37,64 +47,21 @@ function onClickHammer() {
   return () => {
     app.objects.hammer.visible = false;
 
+    const ms = 300;
+
     setTimeout(() => {
-      app.objects.circle01.y -= 100;
-      app.objects.carpet01.y -= 100;
-
-      app.objects.circle01.alpha = 0;
-      app.objects.carpet01.alpha = 0;
-      app.objects.circle01.visible = true;
-      app.objects.carpet01.visible = true;
-
-      const interval = setInterval(() => {
-        app.objects.circle01.alpha += 0.02;
-        app.objects.carpet01.alpha += 0.02;
-        app.objects.circle01.y += 2;
-        app.objects.carpet01.y += 2;
-        if (app.objects.circle01.alpha > 1) {
-          clearInterval(interval);
-        }
-      }, 0);
+      appearance(app.objects.circle01, ms / 16, 67);
+      appearance(app.objects.carpet01, ms / 16, 67);
     }, 0);
 
     setTimeout(() => {
-      app.objects.circle02.y -= 100;
-      app.objects.carpet02.y -= 100;
-
-      app.objects.circle02.alpha = 0;
-      app.objects.carpet02.alpha = 0;
-      app.objects.circle02.visible = true;
-      app.objects.carpet02.visible = true;
-
-      const interval = setInterval(() => {
-        app.objects.circle02.alpha += 0.02;
-        app.objects.carpet02.alpha += 0.02;
-        app.objects.circle02.y += 2;
-        app.objects.carpet02.y += 2;
-        if (app.objects.circle02.alpha > 1) {
-          clearInterval(interval);
-        }
-      }, 0);
+      appearance(app.objects.circle02, ms / 16, 67);
+      appearance(app.objects.carpet02, ms / 16, 67);
     }, 200);
 
     setTimeout(() => {
-      app.objects.circle03.y -= 100;
-      app.objects.carpet03.y -= 100;
-
-      app.objects.circle03.alpha = 0;
-      app.objects.carpet03.alpha = 0;
-      app.objects.circle03.visible = true;
-      app.objects.carpet03.visible = true;
-
-      const interval = setInterval(() => {
-        app.objects.circle03.alpha += 0.02;
-        app.objects.carpet03.alpha += 0.02;
-        app.objects.circle03.y += 2;
-        app.objects.carpet03.y += 2;
-        if (app.objects.circle03.alpha > 1) {
-          clearInterval(interval);
-        }
-      }, 0);
+      appearance(app.objects.circle03, ms / 16, 67);
+      appearance(app.objects.carpet03, ms / 16, 67);
     }, 400);
 
     app.objects.circle01.on(
@@ -124,7 +91,7 @@ function onClickHammer() {
   };
 }
 
-function onClickCarpet(sprite, btn, stairs) {
+function onClickCarpet(choosed, btn, stairs) {
   return () => {
     app.objects.oldStairs.visible = false;
     app.objects.newStairs01.visible = false;
@@ -139,9 +106,22 @@ function onClickCarpet(sprite, btn, stairs) {
     app.objects.btnOk02.visible = false;
     app.objects.btnOk03.visible = false;
 
-    stairs.visible = true;
+    choosed.visible = true;
+    choosed.alpha = 0;
     btn.visible = true;
-    sprite.visible = true;
+    btn.alpha = 0;
+    stairs.visible = true;
+
+    requestAnimationFrame(update);
+
+    function update() {
+      choosed.alpha += 0.02;
+      btn.alpha += 0.02;
+
+      if (choosed.alpha < 1) {
+        requestAnimationFrame(update);
+      }
+    }
   };
 }
 
@@ -162,6 +142,53 @@ function onClickOk() {
   app.objects.btnOk02.visible = false;
   app.objects.btnOk03.visible = false;
 
-  app.objects.overlay.visible = true;
-  app.objects.continueBanner.visible = true;
+  setTimeout(() => {
+    app.objects.overlay.visible = true;
+    app.objects.continueBanner.visible = true;
+  }, 1000);
+}
+
+/////////////////////////
+function appearance(object, frames = 20) {
+  object.pivot.set(object.width / 2, object.height / 2);
+  object.x += object.width / 2;
+  object.y += object.height / 2;
+
+  object.scale.x = 0.75;
+  object.scale.y = 0.75;
+
+  object.alpha = 0;
+  object.visible = true;
+
+  requestAnimationFrame(update);
+  function update() {
+    object.alpha += 1 / frames;
+    object.scale.x += 0.25 / frames;
+    object.scale.y += 0.25 / frames;
+
+    if (object.alpha < 1) {
+      requestAnimationFrame(update);
+    }
+  }
+}
+
+function animate({timing, draw, duration}) {
+
+  let start = performance.now();
+
+  requestAnimationFrame(function animate(time) {
+    // timeFraction изменяется от 0 до 1
+    let timeFraction = (time - start) / duration;
+    if (timeFraction > 1) timeFraction = 1;
+
+    // вычисление текущего состояния анимации
+    let progress = timing(timeFraction);
+
+    draw(progress); // отрисовать её
+
+    if (timeFraction < 1) {
+      requestAnimationFrame(animate);
+    }
+
+  });
 }
